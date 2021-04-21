ceb758578ba487036cbb1fc7c089af737d32292a
tuan-anh.le <tuan-anh.le@cerema.fr>
2021-04-21 19:30:48 +0200

    commit exercise 12 - custom validators in forms
    Co-authored-by: JB Nizet <jnizet@users.noreply.github.com>

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'pr-register',
@@ -7,25 +9,53 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registrationFailed: boolean;
   loginCtrl: FormControl;
   passwordCtrl: FormControl;
+  confirmPasswordCtrl: FormControl;
   birthYearCtrl: FormControl;
   userForm: FormGroup;
+  passwordForm: FormGroup;
 
-  constructor(fb: FormBuilder) {
-    this.loginCtrl = fb.control('', Validators.required);
-    this.passwordCtrl = fb.control('', Validators.required);
-    this.birthYearCtrl = fb.control('', Validators.required);
-    this.userForm = fb.group({
+  static passwordMatch(group: AbstractControl): { matchingError: true } | null {
+    const password = group.get('password').value;
+    const confirm = group.get('confirmPassword').value;
+
+    return password === confirm ? null : { matchingError: true };
+  }
+
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loginCtrl = this.fb.control('', [Validators.required, Validators.minLength(3)]);
+    this.passwordCtrl = this.fb.control('', Validators.required);
+    this.confirmPasswordCtrl = this.fb.control('', Validators.required);
+    this.birthYearCtrl = this.fb.control('', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]);
+
+    this.passwordForm = this.fb.group(
+      {
+        password: this.passwordCtrl,
+        confirmPassword: this.confirmPasswordCtrl
+      },
+      {
+        validators: RegisterComponent.passwordMatch
+      }
+    );
+
+    this.userForm = this.fb.group({
       login: this.loginCtrl,
-      password: this.passwordCtrl,
+      passwordForm: this.passwordForm,
       birthYear: this.birthYearCtrl
     });
   }
 
-  ngOnInit(): void {}
-
   register(): void {
     console.log(this.userForm.value);
+    this.userService
+      .register(this.userForm.value.login, this.userForm.value.passwordForm.password, this.userForm.value.birthYear)
+      .subscribe({
+        next: () => this.router.navigate(['/']),
+        error: () => (this.registrationFailed = true)
+      });
   }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserModel } from './models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  register(loginVal: string, passwordVal: string, birthYearVal: number): Observable<UserModel> {
+    const params = { login: loginVal, password: passwordVal, birthYear: birthYearVal };
+    return this.http.post<UserModel>('https://ponyracer.ninja-squad.com/api/users', params);
+  }
+}
